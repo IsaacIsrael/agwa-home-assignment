655b86ea480b7527136e2d0b14274ac57350e523
isaac israel <email.iisrael@gmail.com>
2021-09-30 18:40:29 +0300

    ⛓  refactoring styles and sizes utils

--- utils/sizes.js
+++ utils/sizes.js
@@ -2,6 +2,7 @@ const Sizes = {
   gutter: 16,
   smallFont:  15,
   regularFont:  20,
+  bigFont:  36,
   buttonFont: 20,
 }
 
--- utils/styles.js
+++ utils/styles.js
@@ -1,7 +1,6 @@
 const Colors = {
     primary: '#6EC171',
     secondary: '#A1CC3A',
-    textColor: '#475B4F',
     accent: '#4CC4E0',
     red: '#cc0000',
     white: '#FFFFFF'
