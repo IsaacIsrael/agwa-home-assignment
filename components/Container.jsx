a8e600f3dae214bb3ce0f07110b70083dc689b8b
isaac israel <email.iisrael@gmail.com>
2021-09-30 18:38:56 +0300

    ⛓  refactoring Container component

--- components/Container.jsx
+++ components/Container.jsx
@@ -6,6 +6,7 @@ import Sizes from '../utils/sizes';
 import Touchable from "./buttons/Touchable";
 import { View } from "react-native";
 import BackButton from "./buttons/BackButton";
+import HeaderText from "./texts/HeaderText";
 
 const styles = StyleSheet.create({
 	container: {
@@ -15,9 +16,15 @@ const styles = StyleSheet.create({
 		backgroundColor: "white",
 	},
 	header:{
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
 		paddingHorizontal:  Sizes.gutter * 0.5,
 		paddingBottom:  Sizes.gutter * 0.5,
 		height: Sizes.gutter * 2,
+	},
+	headerEdgeStyle:{
+		width: 22,
 	},
 	content:{
 		flex: 1,
@@ -27,12 +34,16 @@ const styles = StyleSheet.create({
 	}
 });
 
-const Container = ({ style , children, backButton= false, onBack}) => {
+const Container = ({ style , title, children, backButton= false, onBack}) => {
 	return (
 		<SafeAreaView style={styles.container}>
       <StatusBar />
 			<View style={styles.header}>
-				{backButton && <BackButton onBack={onBack}/>}
+				{backButton 
+				? <BackButton onBack={onBack} buttonStyle={styles.headerEdgeStyle} /> 
+				: < View style={styles.headerEdgeStyle} /> }
+				<HeaderText>{title}</HeaderText>
+				<View style={styles.headerEdgeStyle}/>
 			</View>
 			<View style={[styles.content,style]}>
 				{children}
