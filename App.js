a854f9257b58e991e041fd61b9c5b8a05a8a3d75
isaac israel <email.iisrael@gmail.com>
2021-09-30 08:29:17 +0300

    ⛓  refactoring App

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { Provider } from 'react-redux';
 import React from 'react';
+import { Provider } from 'react-redux';
 import store from './Store/store'
 import AppNavigator from './navigation/AppNavigator';
 
 
-export default App = () => {
-  return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
-  );
-}
+
+export default App = () => (
+  <Provider store={store}>
+    <AppNavigator />
+  </Provider>
+);
+
