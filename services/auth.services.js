12dfd9adb996272e998bb63c282e3831f9534450
isaac israel <email.iisrael@gmail.com>
2021-09-30 08:27:58 +0300

    create auth service

---
+++ services/auth.services.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+
+const KEY = '?key=AIzaSyDLTrlLmj_dFKOPI74doQ2rzuWimkIwLcA';
+const HOST = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+// const URL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDLTrlLmj_dFKOPI74doQ2rzuWimkIwLcA"; 
+// const URL = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDLTrlLmj_dFKOPI74doQ2rzuWimkIwLcA",
+
+
+class AuthService {
+  
+  login({email, password}){
+    const url = `${HOST}signInWithPassword${KEY}`;
+    return axios.post(url, {
+      email,
+      password,
+      returnSecureToken: true,
+    })
+    .then(response => response.data)
+    .catch(error => error.response.data);
+  }
+
+  signUp({email, password}){
+    const url = `${HOST}signUp${KEY}`;
+    return axios.post(url, {
+      email,
+      password,
+      returnSecureToken: true,
+    })
+    .then(response => response.data)
+    .catch(error => error.response.data);
+  }
+
+}
+
+const authService = new AuthService();
+export default authService
