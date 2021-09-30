0f7cd75a6db0ac44557ee693c0235d2f873afea9
isaac israel <email.iisrael@gmail.com>
2021-09-30 14:58:10 +0300

    ⛓  refactoring LogIn view

--- Screens/LoggedOut/LogIn.jsx
+++ Screens/LoggedOut/LogIn.jsx
@@ -1,61 +1,38 @@
 import React, { useState } from "react";
-import {
-	StyleSheet,
-	View,
-	Keyboard,
-} from "react-native";
-import { useDispatch } from "react-redux";
+import { StyleSheet, Keyboard, ActivityIndicator } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 import { Ionicons } from "@expo/vector-icons";
-import { SafeAreaView } from "react-native-safe-area-context";
 
+import Container from '../../components/Container';
 import Input from '../../components/inputs/Input';
-import authService from '../../services/auth.services';
 import ErrorText from '../../components/texts/ErrorText';
+import StickBottomContainer from "../../components/StickBottomContainer";
 import Button from "../../components/buttons/Button";
 
 import Colors from "../../utils/styles";
 import Sizes from '../../utils/sizes';
+import { Creators as userCreator } from '../../Store/duckers/user'	
 
-import { isValidEmail } from "../../utils/helper";
-import { log_in } from "../../Store/Actions/auth";
-import StickBottomContainer from "../../components/StickBottomContainer";
-import Container from '../../components/Container';
-import { StatusBar } from "expo-status-bar";
 import withInternetVerification from "../../hoc/withInternetVerification";
 
 const LogIn = ({ navigation }) => {
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
-	const [errorMessage, setErrorMessage] = useState("");
+
+	const isFetching = useSelector(({user}) => user.fetching);
+	const errorMessage = useSelector(({user}) => user.error);
 
 	const dispatch = useDispatch();
 
 
 	const onLogInPress = async () => {
 		Keyboard.dismiss();
-		// TODO:  Move this logic for action
-		if (!password || !email) {
-			setErrorMessage("Please fill all fields");
-			return;
-		}  
-		
-		if (!isValidEmail(email)) {
-			setErrorMessage("Please enter a valid email");
-			return;
-		}
 			
-		const data = await authService.login({ email, password});
-		const { registered, email: userId , localId :  firebaseUserId} =  data ;
-		
-		if (!registered) {
-			setErrorMessage("Please Sign Up");
-			return;
-		} 
-		
-		dispatch( log_in({ userId, firebaseUserId, }) );
+		dispatch( userCreator.logIn(email, password ));
 	};
 
 	const onSignInPress = () => {
+		dispatch( userCreator.reset());
 		navigation.navigate("signUp");
 	};
 
@@ -76,6 +53,13 @@ const LogIn = ({ navigation }) => {
 			/>
 			{!!errorMessage && ( <ErrorText style={styles.errorText} >{errorMessage}</ErrorText> )}
 			
+			<ActivityIndicator 
+				color={Colors.primary} 
+				size="large" 
+				animating={isFetching}
+				style={styles.loading}
+				/>
+
 			<StickBottomContainer>
 				<Button
 					title='Log In'
@@ -93,10 +77,13 @@ const LogIn = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
+	container:{
+		paddingTop: Sizes.gutter * 4,
+	},
 	icon: {
 		color: Colors.primary,
 		alignSelf: 'center',
-		marginBottom: Sizes.gutter * 2,
+		marginBottom: Sizes.gutter * 4,
 	},
 	inputContainer:{
 		marginBottom: Sizes.gutter,
@@ -106,6 +93,9 @@ const styles = StyleSheet.create({
 	},
 	button:{
 		marginBottom: Sizes.gutter,
+	},
+	loading:{
+		marginTop: 20,
 	}
 });
 
